a1e0007a5caa7915789b5de99aeccddc9e34be9e
deepak <deepak@mindshiftapps.com>
2016-03-23 09:54:08 +0530

    23 march 2016 XMpp linker issues fixed

---
+++ Learniat Teacher/Learniat Teacher/Classes/Common/XMPP/Utilities/RFImageToDataTransformer.h
@@ -0,0 +1,18 @@
+
+/*
+     File: RFImageToDataTransformer.h
+ Abstract: A value transformer, which transforms a UIImage or NSImage object into an NSData object.
+ 
+ Based on Apple's UIImageToDataTransformer
+ 
+ //  Copyright (c) 2014 Mindshift applications and solutions Private Ltd. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+
+@interface RFImageToDataTransformer : NSValueTransformer {
+
+}
+
+@end
